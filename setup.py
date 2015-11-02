c8fe4d773b902bf75d77de2b65ef8028798e93c2
Kirill Kniazev <kirill.kniazev@divio.ch>
2015-11-02 18:45:26 +0200

    Update setup.py, prepare for 1.0.0 release

--- setup.py
+++ setup.py
@@ -2,6 +2,25 @@
 from setuptools import setup, find_packages
 from aldryn_addons import __version__
 
+REQUIREMENTS = [
+    # intentionally left out as a workaround for pips ignorance regarding
+    # exact versions to install
+    # 'Django',
+    'django-getenv',
+]
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+]
 
 setup(
     name='aldryn-addons',
@@ -11,10 +30,8 @@ setup(
     author_email='info@divio.ch',
     url='https://github.com/aldryn/aldryn-addons',
     packages=find_packages(),
-    install_requires=(
-        # 'Django',  # intentionally left out as a workaround for pips ignorance regarding exact versions to install
-        'django-getenv',
-    ),
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
     include_package_data=True,
     zip_safe=False,
 )
